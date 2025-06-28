393348756a54ab0b6b1fd9aeca01d39b7f2ce25d
Sophiasy2008 <hifamironzz@gmail.com>
2025-06-29 00:58:40 +0800

    Update nolaghub_nokey.lua

--- nolaghub_nokey.lua
+++ nolaghub_nokey.lua
@@ -1,12 +1,12 @@
--- Load the GrowAGarden Spawner first
-local Spawner = loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
-Spawner.Load()
-
--- Then load Sophia's additional script
+-- Load Sophia's script first
 local success, err = pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Sophiasy2008/script/refs/heads/main/gag2"))()
 end)
 
 if not success then
-    warn("[Sophia Script Error]:", err)
+    warn("[Sophia gag2 Script Error]:", err)
 end
+
+-- Then load the GrowAGarden Spawner script
+local Spawner = loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
+Spawner.Load()
